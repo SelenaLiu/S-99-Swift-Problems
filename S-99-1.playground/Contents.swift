2009919e873091d736fc332c66fa0cfab190a55b
Selena Liu <selenaliu@Selenas-MacBook.local>
2018-04-28 13:37:27 -0700

    S-99: 1

---
+++ S-99-1.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+let lastIndex = list.count - 1
+print(list[lastIndex])
+
+
