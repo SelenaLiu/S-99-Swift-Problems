d20266505bd68695411a45d84bc161bb76cd3421
Selena Liu <selenaliu@Selenas-MacBook.local>
2018-05-05 22:27:13 -0700

    Extension of S-99-9, Run-length encoding of a list

---
+++ S-99-10.playground/Contents.swift
@@ -0,0 +1,53 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let myList = ["a", "a", "a", "a", "b", "c", "c", "a", "a", "d", "e", "e", "e", "e"]
+
+func isEqual(a: Any, b: Any) -> Bool? {
+    guard let a = a as? String, let b = b as? String else {
+        return nil
+    }
+    
+    return a == b
+}
+
+func pack(list: [Any]) -> [[Any]]? {
+    var finalList: [[Any]] = []
+    var startIndex = 0
+    var endIndex = 0
+    while startIndex < list.count && endIndex < list.count {
+        if isEqual(a: list[startIndex], b: list[endIndex])! {
+            endIndex += 1
+        } else {
+            var miniList: [Any] = []
+            for i in startIndex..<endIndex {
+                miniList.append(list[i])
+            }
+            finalList.append(miniList)
+            startIndex = endIndex
+        }
+    }
+    return finalList
+}
+
+func encode(list: [Any]) -> [Any] {
+    let packedList = pack(list: list)
+    var finalList: [Any] = []
+    
+    for array in packedList! {
+        if array.count > 1 {
+            let pair = [array.count, array[0]]
+            finalList.append(pair)
+        } else {
+            finalList.append(array[0])
+        }
+    }
+    
+    return finalList
+}
+
+encode(list: myList)
+
+
+
