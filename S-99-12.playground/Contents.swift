7cda24f43da6b15be90d9b831237680374bbba36
Selena Liu <selenaliu@Selenas-MacBook.local>
2018-05-09 10:57:41 -0700

    Decode list into single array

--- S-99-12.playground/Contents.swift
+++ S-99-12.playground/Contents.swift
@@ -2,4 +2,26 @@
 
 import UIKit
 
-var str = "Hello, playground"
+let encodedList = [[4, "a"], [1, "b"], [2, "c"], [2, "a"], [1, "d"], [4, "e"]]
+
+func isEqual(a: Any, b: Any) -> Bool? {
+    guard let a = a as? String, let b = b as? String else {
+        return nil
+    }
+    
+    return a == b
+}
+
+func decode(list: [[Any]]) -> [Any] {
+    var finalList: [Any] = []
+    
+    for item in list {
+        for _ in list[0] {
+            finalList.append(item[1])
+        }
+    }
+    return finalList
+}
+
+decode(list: encodedList)
+
