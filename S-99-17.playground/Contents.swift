a0780fc115d35a13a451088a6d43f9663903c7ac
Selena Liu <selenaliu@Selenas-MacBook.local>
2018-05-13 00:18:40 -0700

    Split a list into two parts

--- S-99-17.playground/Contents.swift
+++ S-99-17.playground/Contents.swift
@@ -2,4 +2,21 @@
 
 import UIKit
 
-var str = "Hello, playground"
+let myList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"]
+
+func split(n: Int, list: [Any]) -> ([Any], [Any]) {
+    var inputtedList = list
+    var listOne: [Any] = []
+    var listTwo: [Any] = []
+    
+    for i in 0..<(n-1) {
+        listOne.append(list[i])
+        inputtedList.remove(at: i)
+    }
+    listTwo = inputtedList
+    
+    return (listOne, listTwo)
+}
+
+split(n: 3, list: myList)
+
